38a97236f24ae4fa6455483b6875a6c370daef93
Gulamwaris <gwarsi70@gmail.com>
2021-09-30 13:00:26 +0530

    Added podile dependencies

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 /**
  * @format
  */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+ import {Buffer} from 'buffer';
+ global.Buffer = Buffer;
+ import {AppRegistry} from 'react-native';
+ import App from './App';
+ import {name as appName} from './app.json';
+ 
+ //Initializing localStorage in react native window
+ let localStorageData = {};
+ window.localStorage = {
+   getItem: key => localStorageData[key],
+  
+   removeItem: key => localStorageData[key],
+   setItem: (key, value) => {
+     localStorageData[key] = value;
+   },
+   clear: () => {
+     localStorageData = {};
+   },
+ };
+ 
+ window.location = {...window.location, href: ''};
+ AppRegistry.registerComponent(appName, () => App);
+ 
